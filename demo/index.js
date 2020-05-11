213f1373f2d5749d06a662eef676b17db6de7b46
Cesar Zapata <cesaros14@gmail.com>
2020-05-11 10:10:58 -0500

    Separate demo, prepare for npm

---
+++ demo/index.js
@@ -0,0 +1,36 @@
+import {
+  create,
+  createFromSelector,
+  append,
+  extend
+} from '../src'
+
+const title = create({
+    type: 'h1',
+    text: 'This is a Title'
+})
+
+const subtitle = create({
+    type: 'h4',
+    text: 'The fantabulous subtitle',
+    className: ['subtitle']
+})
+
+const container = create({
+    className: ['container', 'container--fluid'],
+    id: 'main-container',
+    'data-argument': 'hello'
+})
+
+
+const button = createFromSelector('button#the-button.button')
+
+const extendedButton = extend(button, {
+    text: 'Click Me!',
+    className: ['button--primary'],
+    'data-toggle': 'modal'
+})
+
+append(container, [title, subtitle, extendedButton])
+
+document.body.appendChild(container);
--- src/index.js
+++ src/index.js
@@ -1,36 +1,56 @@
-import {
+import { createParser } from 'scalpel';
+
+function create({ type, ...attributes }) {
+  const element = document.createElement(type ? type : 'div')
+
+  return extend(element, attributes)
+}
+
+function parseSelector(selector) {
+  const parser = createParser()
+  const parsed = parser.parse(selector)[0].body
+  const processed = parsed.slice().reduce((acc, prop) => {
+    if (acc.hasOwnProperty(prop.type)) {
+      acc[prop.type].push(prop.name)
+    } else {
+      acc[prop.type] = [prop.name]
+    }
+
+    return acc
+  }, {})
+
+  return processed
+}
+
+function createFromSelector(selector) {
+  const attributes = parseSelector(selector)
+
+  return create({
+    type: attributes.typeSelector[0],
+    className: attributes.classSelector,
+    id: attributes.idSelector[0]
+  })
+}
+
+function append(element, children) {
+  children.forEach(child => element.appendChild(child))
+}
+
+function extend(element, { className = [], children =[], text, ...attributes }) {
+  const newElement = element.cloneNode(true)
+  const textNode = text ? [document.createTextNode(text)] : []
+
+  newElement.classList.add(...className)
+  append(newElement, [...textNode, ...children])
+
+  Object.entries(attributes).forEach(attr => newElement.setAttribute(attr[0], attr[1]))
+
+  return newElement
+}
+
+export {
   create,
   createFromSelector,
   append,
   extend
-} from './dometizer'
-
-const title = create({
-    type: 'h1',
-    text: 'This is a Title'
-})
-
-const subtitle = create({
-    type: 'h4',
-    text: 'The fantabulous subtitle',
-    className: ['subtitle']
-})
-
-const container = create({
-    className: ['container', 'container--fluid'],
-    id: 'main-container',
-    'data-argument': 'hello'
-})
-
-
-const button = createFromSelector('button#the-button.button')
-
-const extendedButton = extend(button, {
-    text: 'Click Me!',
-    className: ['button--primary'],
-    'data-toggle': 'modal'
-})
-
-append(container, [title, subtitle, extendedButton])
-
-document.body.appendChild(container);
+}
